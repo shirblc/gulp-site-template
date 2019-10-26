8d899b53ccc88dfaa49d183f69fed0f7bb991025
Shir Bar Lev <shirblc@gmail.com>
2019-10-26 17:43:34 +0300

    Updated Gulp syntax for Gulp 4.
    *All tasks are now functions.
     *All functions are exported so that Gulp knows they're tasks.
    *The watcher now works.

--- gulpfile.js
+++ gulpfile.js
@@ -8,58 +8,88 @@ const sourcemaps = require("gulp-sourcemaps");
 const order = require("gulp-order");
 
 //copies the html to the disribution folder
-gulp.task("copy-html", function() {
-	gulp.src("index.html")
-	.pipe(gulp.dest('./dist'))
-});
+function copyHtml()
+{
+	return gulp
+		.src("index.html")
+		.pipe(gulp.dest("./dist"));
+};
 
 //copies the images folder to the distribution folder
-gulp.task("copy-imgs", function() {
-	gulp.src("img/*")
-	.pipe(gulp.dest("dist/img"))
-});
+function copyImgs()
+{
+	return gulp
+		.src("img/*")
+		.pipe(gulp.dest("dist/img"));
+}
 
 //sets gulp to add prefixes with Autoprefixer after Dreamweaver outputs the Sass filee to CSS
 //once the prefixer finishes its job, outputs the file to the distribution folder
-gulp.task("styles", function() {
-	gulp.src("css/*.css")
-	.pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
-	.pipe(gulp.dest('./dist/css'))
-});
+function styles()
+{
+	return gulp
+		.src("css/*.css")
+		.pipe(postcss([autoprefixer()]))
+		.pipe(gulp.dest("./dist/css"));
+}
 
 //deals with concating the scripts while in development mode
-gulp.task("scripts", function() {
-	gulp.src("js/*.js")
-	.pipe(sourcemaps.init())
-	.pipe(babel({presets: ['@babel/preset-env']}))
-	.pipe(order([]))
-	.pipe(concat('all.js'))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest("dist/js"))
-});
+function scripts()
+{
+	return gulp
+		.src("js/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(babel({presets: ['@babel/preset-env']}))
+		.pipe(order([
+		
+		]))
+		.pipe(concat('all.js'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("dist/js"))
+}
 
 //deals with concating the scripts while in production mode
-gulp.task("scripts-dist", function() {
-	gulp.src("js/*.js")
-	.pipe(sourcemaps.init())
-	.pipe(babel({presets: ['@babel/preset-env']}))
-	.pipe(order([]))
-	.pipe(concat('all.js'))
-	.pipe(uglify())
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest("dist/js"))
-});
+function scriptsDist()
+{
+	return gulp
+		.src("js/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(babel({presets: ['@babel/preset-env']}))
+		.pipe(order([
+		
+		]))
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("dist/js"))
+}
 
 //prepare for distribution
-gulp.task("dist", gulp.parallel(
-	"copy-html",
-	"copy-imgs",
-	"styles",
-	"scripts-dist"
-));
+function dist()
+{
+	return gulp
+		.parallel(
+			copyHtml,
+			copyImgs, 
+			styles, 
+			scriptsDist
+		);
+}
 
-//default task
-gulp.task("default", gulp.parallel("styles", "copy-html", "copy-imgs", "scripts", "scripts-dist"), function() {
-	gulp.watch('css/*.css', ['styles']);
-	gulp.watch('/index.html', ['copy-html']);
-});
+//watch files for changes and then run the appropriate tasks
+function watch()
+{
+	gulp.watch('/index.html', copyHtml);
+	gulp.watch('img/*', copyImgs);
+	gulp.watch('css/*.css', styles);
+	gulp.watch('/js/*.js', scripts);
+}
+
+//exports for gulp to recognise them as tasks
+exports.copyHtml = copyHtml;
+exports.copyImgs = copyImgs;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.scriptsDist = scriptsDist;
+exports.dist = dist;
+exports.watch = watch;
